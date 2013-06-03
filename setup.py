17f6d4fbdc3ff6271d5d3d4551ad2b5cfada71aa
Dougal Sutherland <dougal@gmail.com>
2013-06-03 14:19:43 -0400

    make an actual release

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ except ImportError:
 
 setup(
     name='vlfeat-ctypes',
-    version='0.1.0dev',
+    version='0.1.0',
     author='Dougal J. Sutherland',
     author_email='dougal@gmail.com',
     packages=['vlfeat'],
     url='https://github.com/dougalsutherland/vlfeat-ctypes',
     description='A minimal ctypes-based port of some vlfeat functions.',
     long_description=open('README.rst').read(),
-    license='LICENSE.txt',
+    license='BSD',
     zip_safe=False,
 )
